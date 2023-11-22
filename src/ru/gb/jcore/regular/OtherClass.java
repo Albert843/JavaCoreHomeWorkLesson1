daeac26f6f968e2d020ba2fbacba33d39b4c362f
Albert843 <nuralbert@yandex.ru>
2023-11-23 00:27:31 +0300

    добавлены файлы

---
+++ src/ru/gb/jcore/regular/OtherClass.java
@@ -0,0 +1,48 @@
+package ru.gb.jcore.regular;
+
+/**
+ * Другой класс приложения, его назначение,
+ * способы взаимодействия с ним.
+ */
+public class OtherClass {
+    /**
+     * Метод сложения двух целых чисел
+     * @param a первое слагаемое
+     * @param b второе слагаемое
+     * @return сумма.
+     */
+    public  static int add(int a, int b) {
+        return a + b;
+    }
+
+    /**
+     * Метод вычитания двух чисел
+     * @param a уменьшаемое
+     * @param b вычитаемое
+     * @return разность
+     */
+    public  static int sub(int a, int b) {
+        return a - b;
+    }
+
+    /**
+     * Метод умножения двух чисел
+     * @param a первый множитель
+     * @param b второй множитель
+     * @return произведение
+     */
+    public  static int mul(int a, int b) {
+        return a * b;
+    }
+
+    /**
+     * Метод деления двух чисел
+     * @param a делимое
+     * @param b делитель
+     * @return частное
+     */
+    public  static int div(int a, int b) {
+        return a / b;
+    }
+
+}
--- src/ru/gb/jcore/sample/Main.java
+++ src/ru/gb/jcore/sample/Main.java
@@ -1,5 +1,25 @@
+package ru.gb.jcore.sample;
+
+import ru.gb.jcore.regular.OtherClass;
+import ru.gb.jcore.regular.Decorator;
+
+/**
+ * Главный класс приложения, его назначение,
+ * способы взаимодействия с ним.
+ */
 public class Main {
+    /**
+     * Точка входа в программу, старт программы
+     * @param args стандартные аргументы командной строки.
+     */
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int result = OtherClass.add(2, 2);
+        System.out.println(Decorator.decorate(result));
+        result = OtherClass.sub(2, 2);
+        System.out.println(Decorator.decorate(result));
+        result = OtherClass.mul(2, 2);
+        System.out.println(Decorator.decorate(result));
+        result = OtherClass.div(2, 2);
+        System.out.println(Decorator.decorate(result));
     }
 }
